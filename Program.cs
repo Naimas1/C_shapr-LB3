2e7a6f41f3350a434536da5b7a17d81ddd1ffef5
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-06-15 19:41:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть шестизначне число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int reversedNumber = ReverseNumber(number);
+
+        Console.WriteLine($"Результат: {reversedNumber}");
+
+        Console.ReadLine();
+    }
+
+    static int ReverseNumber(int number)
+    {
+        int reversedNumber = 0;
+
+        while (number > 0) 
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        return reversedNumber;
+    }
+}
